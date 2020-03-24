9a0972761404dd4dc755b965093573a9f1444f78
CJHydraGenZ <cahyasaga321@gmail.com>
2020-03-24 17:21:31 +0800

    restfull api

---
+++ latihan/restfullapi.py
@@ -0,0 +1,52 @@
+from flask import Flask, jsonify, request
+from flask_pymongo import PyMongo
+
+app = Flask(__name__)
+
+app.config['MONGO_DBNAME'] = 'dbrestapi'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/dbrestapi'
+
+mongo = PyMongo(app)
+
+
+@app.route('/col', methods=['GET'])
+def get_all_framework():
+    mycol = mongo.db.restapi
+
+    output = []
+
+    for x in mycol.find():
+        output.append({'nama': x['nama'], 'bahasa': x['bahasa']})
+
+    return jsonify({'result': output})
+
+
+@app.route('/col/<nama>', methods=['GET'])
+def get_one_framework(nama):
+    mycol = mongo.db.restapi
+    x = mycol.find_one({'nama': nama})
+    if x:
+        output = {'nama': x['nama'], 'bahasa': x['bahasa']}
+    else:
+        output = "No result found"
+
+    return jsonify({'result': output})
+
+
+@app.route('/col', methods=['POST'])
+def add_framework():
+    mycol = mongo.db.restapi
+
+    nama = request.json['nama']
+    bahasa = request.json['bahasa']
+
+    mycol_id = mycol.insert({'nama': nama, 'bahasa': bahasa})
+    new_framework = mycol.find_one({'_id': mycol_id})
+    output = {'nama': new_framework['nama'],
+              'bahasa':  new_framework['bahasa']}
+
+    return jsonify({'result': output})
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
